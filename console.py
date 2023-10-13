8a4d36137af1090670b0c3585cd9dd3ef90d4981
Stacy-Muhia <stacymuhiap@gmail.com>
2023-10-13 12:43:08 -0700

    first part of task 7 some more research needed

--- console.py
+++ console.py
@@ -5,9 +5,19 @@ This is a program that contains entry points of the command line intepreter
 
 
 import cmd
+import json
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+
+    def do_create(self, args):
+        """Creates a new instance of BaseModel"""
+
+
+    def do_show(self, args):
+        """ Prints the string representation of an instance based on the class name"""
+
 
     def do_quit(self, args):
         """Quit command to exit the program"""
